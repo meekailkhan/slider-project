ae5e6fbba3c94246caf9bcc6c704833de5033f7e
meekailkhan <meekailkhan123aar@gmail.com>
2024-03-28 02:54:33 +0530

    this is slider project

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ mainContainer.addEventListener("click",function(e){
     if(e.target.tagName !== "A"){
         return
     }
-    if(e.target.classList[0]=== "next"){
-        if(currentTranstlation > -1800){
-            currentTranstlation -= 600;
-            slide.style.transform = `translate(${currentTranstlation}px)`
 
-        }
-    }
-    if(e.target.classList[0]=== "prev"){
-        if(currentTranstlation < 0){
-            currentTranstlation += 600;
-            slide.style.transform = `translate(${currentTranstlation}px)`
-        }
-    }
-    // e.target.classList[0] === "next" ?  slide.style.transform += "translate(-600px)" : null;
-    // e.target.classList[0] === "prev" ? slide.style.transform += "translate(600px)" : null;
+    const isNext = e.target.classList.contains("next");
+    if (e.target.tagName !== "A" || (isNext && currentTranstlation <= -1800) || (!isNext && currentTranstlation >= 0)) return;
+    
+    currentTranstlation += isNext ? -600 : 600;
+    slide.style.transform = `translateX(${currentTranstlation}px)`;
+    // if(e.target.classList[0]=== "next"){
+    //     if(currentTranstlation > -1800){
+    //         currentTranstlation -= 600;
+    //         slide.style.transform = `translate(${currentTranstlation}px)`
+
+    //     }
+    // }
+    // if(e.target.classList[0]=== "prev"){
+    //     if(currentTranstlation < 0){
+    //         currentTranstlation += 600;
+    //         slide.style.transform = `translate(${currentTranstlation}px)`
+    //     }
+    // }
 })
